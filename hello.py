ed39c223538e54feba141901409c9b789e58d7a6
Anil <aky7467@gmail.com>
2017-09-03 14:08:30 -0500

    updated log info

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 import logging
 
+logging.basicConfig(filename="hello.log", format='%(asctime)s-%(levelname)s-%(message)s', level=logging.INFO)
+
 if __name__ == "__main__":
 	greeting = "Git World"
-	print "Hello..{}".format(greeting)
+	#print "Hello..{}".format(greeting)
+	logging.info("Hello..{}".format(greeting))
